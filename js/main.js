af1cfb70f8155b3ebeb2755226ac380d4cf143b0
steve braco <steve.braco@gmail.com>
2021-02-09 02:00:12 +0100

    finish our story

--- js/main.js
+++ js/main.js
@@ -4,4 +4,29 @@ function scrollHeader(){
     // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 300) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener('scroll', scrollHeader);
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+    distance: '100px',
+    reset: false,
+});
+
+sr.reveal(`.logo, .story, .bg-header, .message__bubble-one, .about, .beauty, .keeping`, {
+    duration: 1800,
+    origin: 'top',
+    interval: 10,
+})
+sr.reveal(`.message__bubble-two,.online`, {
+    duration: 2500,
+    origin: 'top',
+    interval: 20,
+})
+
+sr.reveal(`.message__bubble-three,.message__bubble-four`, {
+    duration: 4000,
+    origin: 'top',
+    interval: 20,
+})
+
+
